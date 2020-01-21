93582e354e046434e87bdbeafc2d31f2c3af2e53
Spyros Argalias <sargalias@gmail.com>
2020-01-21 16:51:08 +0000

    38g8xh: config(gatsby-config.js): Replace Roboto font with Raleway

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Roboto:300,400,500,700&display=swap'],
+          families: ['Raleway:400,700'],
         },
       },
     },
