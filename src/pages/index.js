ad69ac1a5a26aed2cb33416287aa1f84e489eac6
Spyros Argalias <sargalias@gmail.com>
2020-01-21 17:01:49 +0000

    38g8xh: feat(pages/index): Add Hero component to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Layout from 'layout';
+import Container from 'components/Container';
 import Hero from 'components/Hero';
 
 const IndexPage = () => (
   <Layout>
-    <Hero />
+    <Container>
+      <Hero />
+    </Container>
   </Layout>
 );
 
