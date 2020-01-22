0bac4b43b121495601f267a7198e575e1eed3c2f
Spyros Argalias <sargalias@gmail.com>
2020-01-22 15:52:08 +0000

    38n27t: feat(components/post/Post): Wrap with TextContainer

--- src/components/post/Post/Post.jsx
+++ src/components/post/Post/Post.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import PostHeader from 'components/post/PostHeader';
 import PostContent from 'components/post/PostContent';
+import TextContainer from 'components/TextContainer';
 import styles from './Post.module.scss';
 
 const Post = ({ frontmatter, html }) => (
-  <article>
-    <PostHeader frontmatter={frontmatter} />
-    <PostContent html={html} parentClass={styles.Post_content} />
-  </article>
+  <TextContainer>
+    <article className={styles.Post}>
+      <PostHeader frontmatter={frontmatter} />
+      <PostContent html={html} parentClass={styles.Post_content} />
+    </article>
+  </TextContainer>
 );
 
 export default Post;
