30794f201cb885d3379f0e44a50d860684d41337
Spyros Argalias <sargalias@gmail.com>
2020-01-27 04:08:15 +0000

    38n1rf: feat(post/postTemplates/PostPreview): Add PostPreview component

--- src/components/post/postAtoms/PostFeaturedImage/PostFeaturedImage.jsx
+++ src/components/post/postAtoms/PostFeaturedImage/PostFeaturedImage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Img from 'gatsby-image';
 
-const PostFeaturedImage = ({ featuredImageData: { fluid } }) => (
-  <Img fluid={fluid} />
+const PostFeaturedImage = ({ featuredImageData: { fluid }, parentClass }) => (
+  <div className={parentClass}>
+    <Img fluid={fluid} />
+  </div>
 );
 
 export default PostFeaturedImage;
