b9340d8d961c5f796c6dad240e923a0fd12c745d
Spyros Argalias <sargalias@gmail.com>
2020-01-27 01:01:16 +0000

    3apbve: feat(uiComponents/Button): Add button / link uiComponent

---
+++ src/uiComponents/Button/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import 'vendor/bulma/Button'; // this must come before our styles
+import styles from './Button.module.scss';
+
+const className = `button is-link ${styles.Button}`;
+
+const Button = ({ children, path, type = 'button' }) =>
+  path ? (
+    <Link to={path} className={className}>
+      {children}
+    </Link>
+  ) : (
+    <button type={type} className={className}>
+      {children}
+    </button>
+  );
+
+export default Button;
