b93273d78eb465a67b8b472d23fb753df5f40666
Spyros Argalias <sargalias@gmail.com>
2020-01-27 22:05:05 +0000

    38g8yb: feat(layout/Layout): Use Navbar

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Base from '_Base';
 import Abstracts from 'Abstracts';
+import Navbar from '../Navbar';
+import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => (
   <>
     <Base />
     <Abstracts />
-    {children}
+    <Navbar />
+    <div role="main" className={styles.Layout_content}>
+      {children}
+    </div>
   </>
 );
 
