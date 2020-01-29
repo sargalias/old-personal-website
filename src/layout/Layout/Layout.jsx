73ba0c7e286bb0dbab448e72e15ce5fc4dfce72a
Spyros Argalias <sargalias@gmail.com>
2020-01-29 16:16:34 +0000

    38n29e: feat(layout/Layout): Use Footer in Layout

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import Base from '_Base';
 import Abstracts from 'Abstracts';
 import Navbar from '../Navbar';
+import Footer from '../Footer';
 import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => (
-  <>
-    <Base />
-    <Abstracts />
-    <Navbar />
-    <div role="main" className={styles.Layout_content}>
-      {children}
+  <div className={styles.Layout}>
+    <div className={styles.Layout_stickyFooterContentWrapper}>
+      <Base />
+      <Abstracts />
+      <Navbar />
+      <div role="main" className={styles.Layout_content}>
+        {children}
+      </div>
     </div>
-  </>
+    <Footer parentClass={styles.Layout_stickyFooterFooter} />
+  </div>
 );
 
 export default Layout;
