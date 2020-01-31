1d0870e50ec33a85dd1ff711e02ff19028ccc478
Spyros Argalias <sargalias@gmail.com>
2020-01-31 02:32:43 +0000

    3cgj6f: feat(configUtils/pathUtils/formBlogPostPathFromSlug): Add case for config with trailing slash

--- src/configUtils/pathUtils/pathUtils.js
+++ src/configUtils/pathUtils/pathUtils.js
@@ -1,5 +1,12 @@
-const formBlogPostPathFromSlug = () => {
-  throw new TypeError('slug needs to be provided');
+import paths from 'config/paths.json';
+
+const { blogPostPathPrefix, doesUrlHaveTrailingSlash } = paths;
+
+const formBlogPostPathFromSlug = slug => {
+  if (!slug) {
+    throw new TypeError('slug needs to be provided');
+  }
+  return `${blogPostPathPrefix}${slug}/`;
 };
 
 export { formBlogPostPathFromSlug };
--- src/configUtils/pathUtils/pathUtils.test.js
+++ src/configUtils/pathUtils/pathUtils.test.js
@@ -1,9 +1,47 @@
 import { formBlogPostPathFromSlug } from './pathUtils';
+import each from 'jest-each';
+
+const mockConfigPaths = ({ blogPostPathPrefix, doesUrlHaveTrailingSlash }) => {
+  jest.doMock(
+    'config/paths.json',
+    () => ({
+      blogPostPathPrefix,
+      doesUrlHaveTrailingSlash,
+    }),
+    { virtual: true },
+  );
+};
 
 describe('pathUtils', () => {
   describe('formBlogPostPathFromSlug', () => {
+    beforeEach(jest.resetModules);
+
     test('should throw TypeError with no slug provided', () => {
       expect(formBlogPostPathFromSlug).toThrow();
     });
+
+    each`
+    slug          | blogPostPathPrefix  | doesUrlHaveTrailingSlash  | expected
+    ${'foo-bar'}  | ${'/blog-foo/'}     | ${true}                   | ${'/blog-foo/foo-bar/'}
+    `.test(
+      `should return $expected when {
+        blogPostPathPrefix: $blogPostPathPrefix,
+        doesUrlHaveTrailingSlash: $doesUrlHaveTrailingSlash,
+        slug: $slug,
+      }`,
+      async ({
+        slug,
+        blogPostPathPrefix,
+        doesUrlHaveTrailingSlash,
+        expected,
+      }) => {
+        mockConfigPaths({ blogPostPathPrefix, doesUrlHaveTrailingSlash });
+        const { formBlogPostPathFromSlug } = await import('./pathUtils');
+
+        const result = formBlogPostPathFromSlug(slug);
+
+        expect(result).toBe(expected);
+      },
+    );
   });
 });
