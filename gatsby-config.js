213368650e3000a3714f223ca36623b91e11f7cb
Spyros Argalias <sargalias@gmail.com>
2020-02-18 05:19:22 +0000

    3eg29t: config(gatsby-config.js): Configure gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: 'https://www.sargalias.com',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -85,6 +86,7 @@ module.exports = {
     gatsbyPluginWebFontLoaderConfig,
     gatsbyTransformerRemarkConfig,
     gatsbyPluginDisqusConfig,
+    'gatsby-plugin-sitemap',
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
